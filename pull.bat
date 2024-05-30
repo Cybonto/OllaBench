fa76d312a70b33ecc41fcd1d96568104b5847ac5
Cybonto <cybonto@theresearch.ninja>
2024-05-30 13:34:17 -0500

    Add more LLM responses

---
+++ pull.bat
@@ -0,0 +1,6 @@
+ollama pull gemma:7b-text-q4_0
+ollama pull yi:9b-chat-v1.5-q4_0
+ollama pull openchat:7b-v3.5-q4_0
+ollama pull stablelm2:12b-q4_0
+ollama pull solar:10.7b-instruct-v1-q4_0
+ollama pull deepseek-llm:7b-chat-q4_0
---
+++ pull.sh
@@ -0,0 +1,7 @@
+ollama pull aya:8b-23-q4_0
+ollama pull llama3:8b-instruct-q4_0
+ollama pull falcon:7b-text-q4_0
+ollama pull llama3:8b-text-q4_0
+ollama pull qwen:7b-q4_0
+ollama pull qwen:4b-chat-v1.5-q4_0
+ollama pull falcon:7b-instruct-q4_0
